cc62e95681f3aeae943aaac8ece85a94a721720d
creation <creation2224@gmail.com>
2025-06-24 15:18:43 +0530

    done for today

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './componnets/Home'
+import EmailComposer from './componnets/Email'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -21,7 +22,8 @@ function App() {
     }}
   />
      {/* Your Content/Components */}
-     <Home/>
+     {/* <Home/> */}
+     <EmailComposer/>
 </div>
 
 </>
