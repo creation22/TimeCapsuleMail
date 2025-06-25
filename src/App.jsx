197abb79d6d6bf9aad5e875df119f888228ad587
creation <creation2224@gmail.com>
2025-06-25 19:30:29 +0530

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import Home from './componnets/Home'
 import EmailComposer from './componnets/Email'
 function App() {
