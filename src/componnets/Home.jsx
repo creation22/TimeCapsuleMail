4e3e5654e18ad2fbb190aed53bc50d32d79cffde
creation <creation2224@gmail.com>
2025-06-25 22:03:35 +0530

    git push use navigate

--- src/componnets/Home.jsx
+++ src/componnets/Home.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import React, { use } from 'react'
 import { Clock, Mail, Heart, Shield, Star, Calendar, Send, CheckCircle, Users, Globe, Sparkles, ArrowRight } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
-  const navigate = (path) => {
-    console.log(`Navigating to: ${path}`);
-  };
+  const navigate = useNavigate()
 
   return (
     <div className='relative z-10 text-gray-900 font-inter'>
