b6a89b3bd840e7c7881b86c1549be89dc6d4b928
creation <creation2224@gmail.com>
2025-06-27 17:36:15 +0530

    footer updates

--- src/componnets/Footer.jsx
+++ src/componnets/Footer.jsx
@@ -1,66 +1,53 @@
 import React from 'react';
-import { Star, Twitter, Linkedin, Github } from 'lucide-react';
+import { Twitter, Linkedin, Github } from 'lucide-react';
 import FooterSupport from './FooterSupport';
 
 const Footer = () => {
   return (
-    <div className='bg-black text-white py-8 px-6 font-serif'>
-      <div className='container mx-auto flex flex-col md:flex-row items-center justify-between gap-6'>
-
-        {/* Star the Repo */}
-        <a
-          href='https://github.com/creation22/TimeCapsuleMail'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='flex items-center gap-2 hover:underline'
-        >
-          <Star className='w-4 h-4' />
-          <span>Star the Repo</span>
-        </a>
+    <div className="bg-black text-white py-10 px-6 font-serif">
+      <div className="container mx-auto flex flex-col items-center gap-8 text-center">
 
         {/* Social Links */}
-        <div className='flex items-center gap-6'>
+        <div className="flex flex-col gap-5">
           <a
-            href='https://x.com/_Creation22'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='flex items-center gap-2 hover:underline'
+            href="https://x.com/_Creation22"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center justify-center gap-3 hover:underline text-lg md:text-xl"
           >
-            <Twitter className='w-4 h-4' />
-            <span className='hidden sm:inline'>Twitter</span>
+            <Twitter className="w-6 h-6 md:w-7 md:h-7" />
+            <span>Twitter</span>
           </a>
 
           <a
-            href='https://linkedin.com/in/ssrajangupta22'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='flex items-center gap-2 hover:underline'
+            href="https://linkedin.com/in/ssrajangupta22"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center justify-center gap-3 hover:underline text-lg md:text-xl"
           >
-            <Linkedin className='w-4 h-4' />
-            <span className='hidden sm:inline'>LinkedIn</span>
+            <Linkedin className="w-6 h-6 md:w-7 md:h-7" />
+            <span>LinkedIn</span>
           </a>
 
           <a
-            href='https://github.com/creation22'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='flex items-center gap-2 hover:underline'
+            href="https://github.com/creation22"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center justify-center gap-3 hover:underline text-lg md:text-xl"
           >
-            <Github className='w-4 h-4' />
-            <span className='hidden sm:inline'>GitHub</span>
+            <Github className="w-6 h-6 md:w-7 md:h-7" />
+            <span>GitHub</span>
           </a>
         </div>
 
-{/* Support Section */}
-<div>
-  <FooterSupport/>
-</div>
-
-
+        {/* Support Section */}
+        <div>
+          <FooterSupport />
+        </div>
 
         {/* Credit */}
-        <div className=' text-center md:text-right text-2xl'>
-          Made by <span className='font-bold'>Srajan Gupta</span>
+        <div className="text-xl md:text-2xl">
+          Made by <span className="font-bold">Srajan Gupta</span>
         </div>
       </div>
     </div>
